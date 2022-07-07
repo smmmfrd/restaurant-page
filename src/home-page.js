4aee6e645ea30f3c9b835029745a0b35ac3167c2
Sam Mumford <smmmfrd@gmail.com>
2022-07-07 15:03:06 -0700

    Add Hours Section and Image to Home Page

--- src/home-page.js
+++ src/home-page.js
@@ -1,9 +1,19 @@
+import introImage from './home-images/pizzeria_shot01.png';
+
 function about(){
 
     let about = document.createElement('div');
     about.id = 'about';
 
-    about.innerHTML = '<p>Here at Fakerino\'s we create the finest pizza for our fellow human beings. All our ingredients are sourced and produced in house!</p> <br><p>Recently it has been reported online that Fakerino\'s has been a part of a conspiracy to abduct, convert, or experiment on people. These claims are unsubstantiated and are likely the work of internet trolls due to our unconventional policies and methods not being accepted in their feeble human minds.</p>'
+    let introSection = document.createElement('div');
+    introSection.innerHTML = '<p>Here at Fakerino\'s we create the finest pizza for our fellow human beings. All our ingredients are sourced and produced in house!</p> <br><p>Recently it has been reported online that Fakerino\'s has been a part of a conspiracy to abduct, convert, or experiment on people. These claims are unsubstantiated and are likely the work of internet trolls due to our unconventional policies and methods not being accepted in their feeble human minds.</p><br>';
+    about.appendChild(introSection);
+
+    let img = new Image();
+    img.src = introImage;
+    img.style = '"width: 256; height: 256;"';
+    img.alt = 'an AI generated image of an alien entity sitting in a pizza parlor';
+    about.appendChild(img);
 
     return about;
 }
@@ -28,6 +38,13 @@ function news(){
     return news;
 }
 
+function hours(){
+    let hours = document.createElement('div');
+    hours.id = 'hours';
+    hours.innerHTML = '<h2>HOURS<h2><strong>Mon.: 8am - 9pm<br>Tues.: 8pm - 9am<br>Wed.: 5am - 1pm<br>Thurs.: 8am - 9pm<br>Scro.: 8em - 9rm<br>Fri.: 1pm - 1am<br>Sat.: 12am - 12pm<br>Sun: Closed<br></strong>'
+    return hours;
+}
+
 function address(){
     let address = document.createElement('div');
     address.id = 'address';
@@ -42,6 +59,7 @@ export default function homePage(){
     // ABOUT
     homeContent.appendChild(about());
     homeContent.appendChild(news());
+    homeContent.appendChild(hours());
     homeContent.appendChild(address());
 
     return homeContent;
