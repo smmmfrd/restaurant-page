069268ebe6d52e65405599854448b55326468462
Sam Mumford <smmmfrd@gmail.com>
2022-07-07 10:38:05 -0700

    Add Images and Build Site from Function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './style.css';
+import ProfilePic from './eldritch_pizza_chef.png';
 
 console.log("heyo");
+
+function homePage(){
+    var holder = document.createElement('div');
+
+    // HEADER
+    let header = document.createElement('div');
+    header.id = 'header';
+    let title = document.createElement('h1');
+    title.textContent = 'Fakerino\'s';
+    header.appendChild(title);
+    holder.appendChild(header);
+
+    // CONTENT
+    let homeContent = document.createElement('div');
+    homeContent.id = 'home-content';
+    homeContent.innerHTML = 'The best and totally real pizza place!<br>Made with reality and pizza ingredients!';
+
+    let profilePicture = new Image();
+    profilePicture.src = ProfilePic;
+    profilePicture.style = '"width: 256px; height: 256px;"';
+    homeContent.appendChild(profilePicture);
+
+    holder.appendChild(homeContent);
+
+    return holder;
+}
+
+function buildPage(){
+    const content = document.querySelector('#content');
+    
+    content.appendChild(homePage());
+}
+
+buildPage();
